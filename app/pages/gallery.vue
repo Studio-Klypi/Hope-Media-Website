d6ff7a0823f7286f1737dd6e3b0d9f368d75c4b1
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-04-26 20:17:18 +0200

    :rocket: setup video cards

--- app/pages/gallery.vue
+++ app/pages/gallery.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts">
-
+import Page from "~/components/composing/Page.vue";
+import Wrapper from "~/components/composing/Wrapper.vue";
+import TestimonialCard from "~/components/pages/book/TestimonialCard.vue";
+import VideoCard from "~/components/pages/gallery/VideoCard.vue";
 </script>
 
 <template>
-  <div>gallery</div>
+  <Page
+    name="gallery"
+    class="pt-10 pb-20 px-6"
+  >
+    <Wrapper class="flex flex-col gap-6">
+      <header>
+        <h1 class="text-3xl font-extrabold">
+          {{ $t("gallery.title") }}
+        </h1>
+      </header>
+
+      <main class="columns-1 @xl/page:columns-2 @3xl/page:columns-3 space-y-4">
+        <VideoCard
+          v-for="i in 10"
+          :key="i"
+        />
+      </main>
+    </Wrapper>
+  </Page>
 </template>
