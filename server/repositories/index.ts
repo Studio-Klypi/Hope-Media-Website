f15d7c77f920a0ae5ac2c9470aab3ba51e80b3ad
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-05-12 10:38:21 +0200

    :rocket: expose web contact model

--- server/repositories/index.ts
+++ server/repositories/index.ts
@@ -3,6 +3,7 @@ import { SessionRepository } from "#server/repositories/entities/session";
 import OTPRepository from "#server/repositories/entities/otp";
 import AuditEntryRepository from "#server/repositories/entities/auditEntry";
 import { ArticleRepository } from "#server/repositories/entities/article";
+import { WebContactRepository } from "#server/repositories/entities/web-contact";
 
 export const UserModel = new UserRepository();
 export const SessionModel = new SessionRepository();
@@ -11,3 +12,5 @@ export const OTPModel = new OTPRepository();
 export const AuditEntryModel = new AuditEntryRepository();
 
 export const ArticleModel = new ArticleRepository();
+
+export const WebContactModel = new WebContactRepository();
